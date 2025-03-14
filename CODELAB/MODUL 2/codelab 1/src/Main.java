2cf9dd07637db37dbc4c3cb0f9dc13253109ad6b
RPXCODE <iqodhulhimam2@gmail.com>
2025-03-14 20:38:30 +0700

    update

---
+++ CODELAB/MODUL 2/codelab 1/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    public static void main(String[] args){
+        Hewan Hewan1 = new Hewan();
+        Hewan Hewan2 = new Hewan();
+
+        Hewan1. Nama = "Kucing";
+        Hewan2. Nama = "Anjing";
+
+        Hewan1. Jenis = "Mamalia";
+        Hewan2. Jenis = "Mamalia";
+
+        Hewan1. Suara = "Nyann~~";
+        Hewan2. Suara = "Woof-Woof!! ";
+
+        Hewan1.tampilkanInfo();
+        Hewan2.tampilkanInfo();
+    }
+}
